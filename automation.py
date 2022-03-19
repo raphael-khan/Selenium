5d012278fc616c8fc5147b449d30e0ee7ced4f74
raphael-khan <raphaelkhan@yahoo.com>
2022-03-19 16:34:59 -0400

    selenium basics

--- automation.py
+++ automation.py
@@ -8,3 +8,17 @@ chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 assert 'Selenium Easy Demo' in chrome_browser.title
 button_text = chrome_browser.find_element_by_class_name('btn-default')
 print(button_text.get_attribute('innerHTML'))
+
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I am amazing !')
+
+button_text.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+
+assert 'I am amazing !' in output_message.text
+
+chrome_browser.close()
