9dc58968a5e073131064b3894185562a4d043d6c
raphael-khan <raphaelkhan@yahoo.com>
2022-03-19 15:47:44 -0400

    innerHTML

--- automation.py
+++ automation.py
@@ -3,3 +3,8 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
 chrome_browser.maximize_window()
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
+assert 'Selenium Easy Demo' in chrome_browser.title
+button_text = chrome_browser.find_element_by_class_name('btn-default')
+print(button_text.get_attribute('innerHTML'))
